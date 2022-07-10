105bdbc6abc28caf2c99e42924564e13cd400af0
rfurtad <robertofurtado94@gmail.com>
2022-07-09 21:33:38 -0300

    initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace Namespace
+{
+    public class TodoItem
+    {
+        public string? Name { get; set; }
+        public bool IsDone { get; set; }
+        public long Id { get; set; }
+
+    }
+}
